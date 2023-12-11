ce48a08b21cbc93c3f11d2a56bca1cc89035f401
naufalafles <zamrinaufalcode@gmail.com>
2023-12-11 23:23:05 +1100

    Setting Up Environment
    Setting up File & Folder Structure, & Developed App.css & Index.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import { Footer, Blog, Possibility, Features, WhatResume, Header} from './containers';
+import { CTA, Brand, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
-    <div>Naufalafels</div>
+    <div className='App'>
+        <div className='gradient__bg'>
+            <Navbar />
+            <Header />
+        </div>
+        <Brand />
+        <WhatResume />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
   )
 }
 
---
+++ src/containers/whatResume/WhatResume.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './whatResume.css';
+
+const WhatResume = () => {
+  return (
+    <div>WhatResume</div>
+  )
+}
+
+export default WhatResume
